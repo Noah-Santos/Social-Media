25c114b93361d54addd9d318f1c4d76918ecf1d9
parsnippity <briannesiemsen@gmail.com>
2023-12-13 15:11:11 -0700

    added dependencies, app.js and connect.js
    set it up so the backend server runs

---
+++ server/app.js
@@ -0,0 +1,29 @@
+const {MongoClient} = require("mongodb");
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const morgan = require("morgan");
+require("dotenv").config();
+const connectDB = require("./db/connect");
+const port = 5000;
+
+app.use(morgan("tiny"));
+app.use(express.json());
+app.use(cors());
+app.use(express.urlencoded({extended: false}));
+
+app.get("/", (req,res) => {
+    res.send("server is working");
+})
+
+const initServer = async() => {
+    try {
+        await connectDB("mongodb+srv://UniversalUser1234:socialMediaUser1234@cluster0.u89rzfi.mongodb.net/Data");
+        app.listen(port, () => {
+            console.log(`Listening on port ${port}`);
+        })
+    } catch(err) {
+        console.log(err);
+    }
+}
+initServer();
