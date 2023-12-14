d63f39353aa3450bafd321c53fb1153ac58460cf
dakotarobot213 <110577432+dakotarobot213@users.noreply.github.com>
2023-12-14 14:55:22 -0700

    Updated Post Model
    Post model's author renamed to authorName
    Post model now has an authorId

--- server/models/Post.js
+++ server/models/Post.js
@@ -1,21 +1,27 @@
 const mongoose = require("mongoose");
-const postSchema = new mongoose.Schema({
-    title: {
-        type:String
-    },
-    id:{
-        type:Number
-    },
-    author:{
-        type:String
-    },
-    image:{
-        type:String
-    },
-    desc:{
-        type:String
-    }
-}, {collection: "Blog-Posts"})
+const postSchema = new mongoose.Schema(
+	{
+		title: {
+			type: String,
+		},
+		id: {
+			type: Number,
+		},
+		authorId: {
+			type: Number,
+		},
+		authorName: {
+			type: String,
+		},
+		image: {
+			type: String,
+		},
+		desc: {
+			type: String,
+		},
+	},
+	{ collection: "Blog-Posts" }
+);
 
 module.exports = mongoose.model("Post", postSchema);
 
