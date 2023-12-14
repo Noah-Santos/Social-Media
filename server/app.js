8f8ab50ac882b84162c6b77e82bd3dad24fe8832
dakotarobot213 <110577432+dakotarobot213@users.noreply.github.com>
2023-12-14 13:32:26 -0700

    Backend env works now
    Created a backend env and hooked it up

--- server/app.js
+++ server/app.js
@@ -1,33 +1,33 @@
-const {MongoClient} = require("mongodb");
+const { MongoClient } = require("mongodb");
 const express = require("express");
 const app = express();
 const cors = require("cors");
 const morgan = require("morgan");
-require("dotenv").config();
+require("dotenv").config({ path: __dirname + `/.env` });
 const connectDB = require("./db/connect");
-const postRoute = require("./routes/postController")
-const userRoute = require("./routes/userController")
+const postRoute = require("./routes/postController");
+const userRoute = require("./routes/userController");
 const port = 5000;
 
 app.use(morgan("tiny"));
 app.use(express.json());
 app.use(cors());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 
-app.get("/", (req,res) => {
-    res.send("server is working");
-})
+app.get("/", (req, res) => {
+	res.send("server is working");
+});
 app.use("/posts", postRoute);
 app.use("/users", userRoute);
 
-const initServer = async() => {
-    try {
-        await connectDB("mongodb+srv://UniversalUser1234:socialMediaUser1234@cluster0.u89rzfi.mongodb.net/Data");
-        app.listen(port, () => {
-            console.log(`Listening on port ${port}`);
-        })
-    } catch(err) {
-        console.log(err);
-    }
-}
+const initServer = async () => {
+	try {
+		await connectDB(process.env.MONGO_URI);
+		app.listen(port, () => {
+			console.log(`Listening on port ${port}`);
+		});
+	} catch (err) {
+		console.log(err);
+	}
+};
 initServer();
