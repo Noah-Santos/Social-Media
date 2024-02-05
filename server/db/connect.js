2755c603d42a7d905485f01cfe3cf8e2b4b44dc3
Noah Santos <111617611+Noah-Santos@users.noreply.github.com>
2024-02-05 16:18:09 -0700

    Connect
    Connecting to MongoDB without the use of mongoose and schemas/models.

--- server/db/connect.js
+++ server/db/connect.js
@@ -9,3 +9,22 @@ const connectDB = (url) => {
 }
 
 module.exports = connectDB;
+
+// const { MongoClient } = require("mongodb");
+// const dbName = 'Social-Media'
+// const client = new MongoClient(url);
+// client.connect(function(err) {
+//     if (err) {
+//         console.error("Failed to connect to the database:", err);
+//         return;
+//     }
+//     console.log("Connected successfully to the MongoDB server");
+
+//     // Access the database
+//     const database = client.db(dbName);
+
+//     // Access the collection
+//     const Posts = database.collection("posts");
+
+//     // Now you can perform operations on the Posts collection
+// });
